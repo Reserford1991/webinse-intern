f9a866ca961a1d3023d64cccfe9f19a5ba6de1dd
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-09 12:44:44 +0200

    INT1S5-12 Added discount grid

---
+++ app/code/local/Webinse/Discount/Block/Adminhtml/Discount/Grid.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_Discount
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * Adminhtml grid block
+ *
+ * @category    Webinse
+ * @package     Webinse_Discount
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+class Webinse_Discount_Block_Adminhtml_Discount_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('discountGrid');
+        $this->setUseAjax(true);
+        $this->setDefaultSort('entity_id');
+        $this->setSaveParametersInSession(true);
+    }
+
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('webinse_discount/discount')->getCollection();
+        $collection  -> joinProducts($collection);
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+        $helper = Mage::helper('webinse_discount');
+
+        $this->addColumn('entity_id', array(
+            'header'        => $helper->__('ID'),
+            'width'         => 50,
+            'align'         => 'right',
+            'filter_index'  => 'main_table.entity_id',
+            'index'         => 'entity_id',
+            'type'          => 'number',
+        ));
+
+        $this->addColumn('product_id', array(
+            'header'        => $helper->__('Product Name'),
+            'align'         => 'left',
+            'index'         => 'value',
+            'filter_index'  => 'value',
+            'type'          => 'text',
+        ));
+
+        $this->addColumn('sku', array(
+            'header'        => $helper->__('Product SKU'),
+            'align'         => 'left',
+            'index'         => 'sku',
+            'filter_index'  => 'sku',
+            'type'          => 'text',
+        ));
+
+        $this->addColumn('qty_products', array(
+            'header'        => $helper->__('QTY'),
+            'align'         => 'left',
+            'index'         => 'qty_products',
+            'filter_index'  => 'qty_products',
+            'type'          => 'text',
+        ));
+
+        $this->addColumn('item_price', array(
+            'header'        => $helper->__('Item Price'),
+            'align'         => 'left',
+            'index'         => 'item_price',
+            'filter_index'  => 'item_price',
+            'type'          => 'text',
+        ));
+
+        $this->addColumn('subtotal', array(
+            'header'        => $helper->__('Subtotal'),
+            'align'         => 'left',
+            'index'         => 'subtotal',
+            'filter_index'  => 'subtotal',
+            'type'          => 'text',
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    protected function _prepareMassaction()
+    {
+        $helper = Mage::helper('webinse_discount');
+
+        $this->setMassactionIdField('entity_id');
+        $this->getMassactionBlock()->setFormFieldName('entity_id');
+
+        $this->getMassactionBlock()->addItem('delete', array(
+            'label'         => $helper->__('Delete'),
+            'url'           => $this->getUrl('*/*/massDelete')
+        ));
+
+        return $this;
+    }
+
+    public function getGridUrl()
+    {
+        return $this->getUrl('*/*/grid', array('_current'=>true));
+    }
+
+}
--- app/code/local/Webinse/Discount/controllers/Adminhtml/Webinse/Discount/IndexController.php
+++ app/code/local/Webinse/Discount/controllers/Adminhtml/Webinse/Discount/IndexController.php
@@ -21,4 +21,42 @@
  */
 class Webinse_Discount_Adminhtml_Webinse_Discount_IndexController extends Mage_Adminhtml_Controller_Action
 {
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->_setActiveMenu('webinse_discount');
+        $this->renderLayout();
+        return $this;
+    }
+
+    public function massDeleteAction()
+    {
+        $helper = Mage::helper('webinse_discount');
+        $Ids=$this->getRequest()->getParam('entity_id');
+        if(!is_array($Ids)) {
+            Mage::getSingleton('adminhtml/session')->addError($helper->__('Please select one or more companies.'));
+        } else {
+            try {
+                $discount = Mage::getModel('webinse_discount/discount');
+                foreach ($Ids as $Id) {
+                    $discount->setId($Id)->delete();
+                }
+                Mage::getSingleton('adminhtml/session')->addSuccess($helper->__('Records were deleted.')
+                );
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+            }
+        }
+
+        $this->_redirect('*/*/index');
+    }
+
+    public function gridAction()
+    {
+        $this->loadLayout();
+        $this->getResponse()->setBody(
+            $this->getLayout()->createBlock('webinse_discount/adminhtml_discount_grid')->toHtml()
+        );
+    }
+
 }
