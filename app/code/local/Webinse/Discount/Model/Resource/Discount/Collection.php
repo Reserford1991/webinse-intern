138cbc9fafe948f0787be132bc9f7e30e1536bdb
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-09 12:36:26 +0200

    INT1S5-11 Created skeleton for discount extention

---
+++ app/code/local/Webinse/Discount/Model/Resource/Discount/Collection.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_Discount
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * Resource Collection model Discount
+ *
+ * @category    Webinse
+ * @package     Webinse_Discount
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+class Webinse_Discount_Model_Resource_Discount_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('webinse_discount/discount');
+    }
+
+    public function joinProducts($collection)
+    {
+        $productName = Mage::getSingleton('eav/config')->getAttribute('catalog_product', 'name');
+        $id = $productName->getId();
+        $collection -> getSelect()
+            ->join(array('product_sku'=>'catalog_product_entity'), 'product_sku.entity_id = main_table.product_id', array('product_sku.sku'));
+        $collection -> getSelect()
+            ->join(array('product_name' => $productName->getBackendTable()), "product_name.entity_id = main_table.product_id AND product_name.attribute_id = $id", array('product_name'=>'product_name.value'));
+        return $collection;
+    }
+
+}
