341a2dcd6fcdf297e9afeafdaf4c9c44d94b5fc4
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-15 15:08:46 +0200

    INT1S5-15 #time 1h created new eav attribute for customer

---
+++ app/code/local/Webinse/Company/sql/webinse_company_setup/upgrade-0.1.0-0.2.0.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_Company
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * Upgrade db script
+ *
+ * @category    Webinse
+ * @package     Webinse_Company
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * @var $this Mage_Core_Model_Resource_Setup
+ */
+$installer = $this;
+$installer->startSetup();
+
+$installer->addAttribute('customer', 'company_id', array(
+    'type' => 'int',
+    'label' => 'Company_id', '
+    input' => 'select',
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'required' => true,
+    'default' => 0,
+    'frontend' => '',
+    'visible_on_front' => false,
+    'backend' => '',
+    'visible' => false,
+));
+
+$installer->endSetup();
