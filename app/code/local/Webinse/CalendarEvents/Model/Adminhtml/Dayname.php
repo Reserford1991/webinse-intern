ad3b443328767dc1cdbb1d98b9e255712163fa1a
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-15 15:37:13 +0200

    INT1S5-21 #time 10m changed dayname.php, system.xml

--- app/code/local/Webinse/CalendarEvents/Model/Adminhtml/Dayname.php
+++ app/code/local/Webinse/CalendarEvents/Model/Adminhtml/Dayname.php
@@ -1,19 +1,36 @@
 <?php
-
-define("ONE", Mage::helper('webinse_calendarevents')->__('Abbreviation'));
-define("TWO", Mage::helper('webinse_calendarevents')->__('Narrow'));
-define("THREE", Mage::helper('webinse_calendarevents')->__('Wide'));
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_CalendarEvents
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * CalendarEvents custom dropdown menu
+ *
+ * @category    Webinse
+ * @package     Webinse_CalendarEvents
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
 
 class Webinse_CalendarEvents_Model_Adminhtml_DayName
 {
-
-
+    const ONE = 0;
+    const TWO = 1;
+    const THREE = 2;
     public function toOptionArray()
     {
         return array(
-            array('value' => 0, 'label' => ONE),
-            array('value' => 1, 'label' => TWO),
-            array('value' => 2, 'label' => THREE),
+            array('value' => self::ONE, 'label' => 'Abbreviation'),
+            array('value' => self::TWO, 'label' => 'Narrow'),
+            array('value' => self::THREE, 'label' => 'Wide'),
         );
     }
 }
