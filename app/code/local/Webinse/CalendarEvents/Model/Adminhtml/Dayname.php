c93f7b79a6f1afc848c4317dcbeabe3206df4346
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-15 12:46:13 +0200

    INT1S5-21 #time 30m changed Dayname.php and system.xml

--- app/code/local/Webinse/CalendarEvents/Model/Adminhtml/Dayname.php
+++ app/code/local/Webinse/CalendarEvents/Model/Adminhtml/Dayname.php
@@ -1,13 +1,19 @@
 <?php
 
+define("ONE", Mage::helper('webinse_calendarevents')->__('Abbreviation'));
+define("TWO", Mage::helper('webinse_calendarevents')->__('Narrow'));
+define("THREE", Mage::helper('webinse_calendarevents')->__('Wide'));
+
 class Webinse_CalendarEvents_Model_Adminhtml_DayName
 {
+
+
     public function toOptionArray()
     {
         return array(
-            array('value' => 0, 'label' => Mage::helper('webinse_calendarevents')->__('Abbreviation')),
-            array('value' => 1, 'label' => Mage::helper('webinse_calendarevents')->__('Narrow')),
-            array('value' => 2, 'label' => Mage::helper('webinse_calendarevents')->__('Wide')),
+            array('value' => 0, 'label' => ONE),
+            array('value' => 1, 'label' => TWO),
+            array('value' => 2, 'label' => THREE),
         );
     }
 }
