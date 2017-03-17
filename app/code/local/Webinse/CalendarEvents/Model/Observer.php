8960ca0b78b130fcae24fa4614114688d37f3431
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-17 16:29:10 +0200

    INT1S5-22 #time 1m changed observer.php

--- app/code/local/Webinse/CalendarEvents/Model/Observer.php
+++ app/code/local/Webinse/CalendarEvents/Model/Observer.php
@@ -4,30 +4,32 @@ class Webinse_CalendarEvents_Model_Observer
 {
     public function cmsField($observer)
     {
-    $helper = Mage::helper('webinse_calendarevents');
-    $groups = Mage::getResourceModel('customer/group_collection')->toOptionHash();
-    $image = Mage::getDesign()->getSkinUrl('images/grid-cal.gif');
-    $format = Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT);
-    $form = $observer->getForm();
-    $fieldset = $form->getElement('base_fieldset');
-    $fieldset->addField('customer_group', 'select', array(
-        'name' => 'customer_group',
-        'label' => $helper->__('Assign to Customer Group'),
-        'options' => $groups,
+        $helper = Mage::helper('webinse_calendarevents');
+        $groups = Mage::getResourceModel('customer/group_collection')->toOptionHash();
+        $image = Mage::getDesign()->getSkinUrl('images/grid-cal.gif');
+        $format = Mage::app()->getLocale()->getDateFormat(Mage_Core_Model_Locale::FORMAT_TYPE_SHORT);
+        $form = $observer->getForm();
+
+        $fieldset = $form->getElement('base_fieldset');
+
+        $fieldset->addField('customer_group', 'select', array(
+            'name' => 'customer_group',
+            'label' => $helper->__('Assign to Customer Group'),
+            'options' => $groups,
         ));
 
-    $fieldset->addField('date_from', 'date', array(
-        'name' => 'date_from',
-        'label' => $helper->__('Date From'),
-        'image' => $image,
-        'format' => $format,
+        $fieldset->addField('date_from', 'date', array(
+            'name' => 'date_from',
+            'label' => $helper->__('Date From'),
+            'image' => $image,
+            'format' => $format,
         ));
 
-    $fieldset->addField('date_to', 'date', array(
-        'name' => 'date_to',
-        'label' => $helper->__('Date To'),
-        'image' => $image,
-        'format' => $format,
+        $fieldset->addField('date_to', 'date', array(
+            'name' => 'date_to',
+            'label' => $helper->__('Date To'),
+            'image' => $image,
+            'format' => $format,
         ));
     }
 }
