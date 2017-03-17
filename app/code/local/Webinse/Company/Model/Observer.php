ead9d6ec8fdd3a02dabdde855b8185e7385d3622
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-17 14:50:20 +0200

    INT1S5-20 #time 3h Created logic to filter customers using companies

---
+++ app/code/local/Webinse/Company/Model/Observer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_Company
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+
+/**
+ * Company observer
+ *
+ * @category    Webinse
+ * @package     Webinse_Company
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+class Webinse_Company_Model_Observer
+{
+    public function Company(Varien_Event_Observer $observer)
+    {
+        $customer = $observer->getCustomer();
+        $mail = $customer->getEmail();
+        $domain_name = substr(strrchr($mail, "@"), 1);
+        $companies = Mage::getModel('webinse_company/company')->getCollection();
+        $restrict = true;
+        foreach ($companies as $thing) {
+            if ($domain_name == $thing->getData('domains')) {
+                $id = $thing->getData('entity_id');
+                $customer->setEntityId($id);
+                $restrict = false;
+           }
+        }
+        if ($restrict) {
+            $error = Mage::helper('webinse_company')->__('Your email is not acceptable in our system.');
+            Mage::throwException($error);
+        }
+
+    }
+}
