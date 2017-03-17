b8d59f82625ef087a7638f04ead61a28c877c113
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-17 16:30:50 +0200

    INT1S5-22 #time 1m changed observer.php

--- app/code/local/Webinse/CalendarEvents/Model/Observer.php
+++ app/code/local/Webinse/CalendarEvents/Model/Observer.php
@@ -1,5 +1,24 @@
 <?php
-
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_CalendarEvents
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * CalendarEvents observer
+ *
+ * @category    Webinse
+ * @package     Webinse_CalendarEvents
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
 class Webinse_CalendarEvents_Model_Observer
 {
     public function cmsField($observer)
