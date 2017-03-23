02bb060be60a33876a5deefdd16c611deded79f3
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-03-23 13:37:55 +0200

    INT1S5-24 #time 5h created e-mail notification for calendar extention

--- app/code/local/Webinse/CalendarEvents/Model/Observer.php
+++ app/code/local/Webinse/CalendarEvents/Model/Observer.php
@@ -51,4 +51,30 @@ class Webinse_CalendarEvents_Model_Observer
             'format' => $format,
         ));
     }
+
+    public function cmsPage($observer)
+    {
+        $emailTemplate  = Mage::getModel('core/email_template');
+        $emailTemplate->loadDefault('notification_mail');
+
+        $pageId =  Mage::app()->getRequest()->getParam('page_id');
+        $pageTitle = Mage::app()->getRequest()->getParam('title');
+        $calendarLink = 'http://magentosite.com/calendarevents/customer/index/';
+
+        $sender = Mage::getStoreConfig('webinse_calendarevents/email/sender_email_identity');
+        $recipientEmail = Mage::getStoreConfig('webinse_calendarevents/email/recipient_email');
+        $recipientName = Mage::getStoreConfig('webinse_calendarevents/email/recipient_name');
+        $emailSubject = Mage::getStoreConfig('webinse_calendarevents/email/email_subject');
+        $senderEmail = Mage::getStoreConfig('trans_email/ident_'.$sender.'/email');
+
+        $emailTemplate->setTemplateSubject($emailSubject);
+        $emailTemplate->setSenderName($sender);
+        $emailTemplate->setSenderEmail($senderEmail);
+
+        $emailTemplateVariables['recipientName']  = $recipientName;
+        $emailTemplateVariables['pageId'] = $pageId;
+        $emailTemplateVariables['pageTitle'] = $pageTitle;
+        $emailTemplateVariables['calendarLink'] = $calendarLink;
+        $emailTemplate->send($recipientEmail, $recipientName, $emailTemplateVariables);
+    }
 }
