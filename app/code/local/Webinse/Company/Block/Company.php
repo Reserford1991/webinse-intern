45f7f835902826395a45f71da49c9bdb4e11efe9
Reserford1991 <vitaliymatveev1991@gmail.com>
2017-04-04 16:43:11 +0300

    INT1S5-32 #time 5h 45m created logic to display company info on customer account

---
+++ app/code/local/Webinse/Company/Block/Company.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Webinse
+ *
+ * PHP Version 5.6.23
+ *
+ * @category    Webinse
+ * @package     Webinse_CalendarEvents
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+/**
+ * Block for Webinse_CalendarEvents
+ *
+ * @category    Webinse
+ * @package     Webinse_CalendarEvents
+ * @author      Webinse Team <info@webinse.com>
+ * @copyright   2017 Webinse Ltd. (https://www.webinse.com)
+ * @license     http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0
+ */
+
+class Webinse_Company_Block_Company extends Mage_Core_Block_Template
+{
+    public function getCustomerCompany()
+    {
+        $customerCompanyId = Mage::getSingleton('customer/session')->getCustomer()->getCompanyId();
+        return $customerCompanyId;
+    }
+
+    public function loadCompany()
+    {
+        $customer = $this->getCustomerCompany();
+        $companyInfo = Mage::getModel('webinse_company/company')->load($customer);
+        return $companyInfo;
+    }
+
+    public function loadAddresses()
+    {
+        $customer = $this->getCustomerCompany();
+        $addressesInfo = Mage::getModel('webinse_company/addresses')->load($customer, 'company_id');
+        return $addressesInfo;
+    }
+
+}
